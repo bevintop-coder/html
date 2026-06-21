301cbee5a060ed360d875722b44ce9134d842ad0
Bevinto Paul <bevintop@gmail.com>
2026-06-21 12:00:10 +0530

    Add files via upload

---
+++ CareerAccelerator.java
@@ -0,0 +1,23 @@
+public class CareerAccelerator {
+
+    public static void main(String[] args) {
+
+        System.out.println("India's Graduate Skill Index");
+        System.out.println("Career Accelerator\n");
+
+        String[] skills = {
+            "Skill Development",
+            "Real Projects",
+            "Interview Preparation",
+            "Career Guidance"
+        };
+
+        for(String skill : skills)
+        {
+            System.out.println(skill);
+        }
+
+        System.out.println("\nContact:");
+        System.out.println("bevintop@gmail.com");
+    }
+}
