7c0dcd80dbaeecd092898a0c841a7f3c6e0855b0
Bevinto Paul <bevintop@gmail.com>
2026-06-21 12:01:06 +0530

    Add files via upload

---
+++ career.py
@@ -0,0 +1,18 @@
+print("India's Graduate Skill Index")
+print("Career Accelerator")
+
+features = [
+    "Skill Development",
+    "Real Projects",
+    "Interview Preparation",
+    "Career Guidance"
+]
+
+print("\nFeatures:")
+
+for item in features:
+    print("-", item)
+
+print("\nContact")
+print("bevintop@gmail.com")
+print("+91-8015208065")
